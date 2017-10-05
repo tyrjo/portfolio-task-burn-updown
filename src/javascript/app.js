899504bc56e416b16fb7cc5332011a26d24ba510
Tyr Johanson <tyr@ca.com>
2017-10-05 13:11:10 -0600

    in-lined configs"

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -34,73 +34,63 @@ Ext.define("PTBUD", {
         this.add({
             xtype: 'rallychart',
             storeType: 'Rally.data.lookback.SnapshotStore',
-            storeConfig: this._getStoreConfig(),
+            
+            /**
+             * Generate the store config to retrieve all snapshots for stories and defects in the current project scope
+             * within the last 30 days
+             */
+            storeConfig: {
+                find: {
+                    _TypeHierarchy: { '$in': ['HierarchicalRequirement', 'Defect'] },
+                    Children: null,
+                    _ProjectHierarchy: this.getContext().getProject().ObjectID,
+                    _ValidFrom: { '$gt': Rally.util.DateTime.toIsoString(Rally.util.DateTime.add(new Date(), 'day', -30)) }
+                },
+                fetch: ['ScheduleState'],
+                hydrate: ['ScheduleState'],
+                sort: {
+                    _ValidFrom: 1
+                },
+                context: this.getContext().getDataContext(),
+                limit: Infinity
+            },
             calculatorType: 'Rally.example.CFDCalculator',
             calculatorConfig: {
                 stateFieldName: 'ScheduleState',
                 stateFieldValues: ['Defined', 'In-Progress', 'Completed', 'Accepted']
             },
-            chartConfig: this._getChartConfig()
-        });
-    },
-
-    /**
-     * Generate the store config to retrieve all snapshots for stories and defects in the current project scope
-     * within the last 30 days
-     */
-    _getStoreConfig: function () {
-        return {
-            find: {
-                _TypeHierarchy: { '$in': ['HierarchicalRequirement', 'Defect'] },
-                Children: null,
-                _ProjectHierarchy: this.getContext().getProject().ObjectID,
-                _ValidFrom: { '$gt': Rally.util.DateTime.toIsoString(Rally.util.DateTime.add(new Date(), 'day', -30)) }
-            },
-            fetch: ['ScheduleState'],
-            hydrate: ['ScheduleState'],
-            sort: {
-                _ValidFrom: 1
-            },
-            context: this.getContext().getDataContext(),
-            limit: Infinity
-        };
-    },
-
-    /**
-     * Generate a valid Highcharts configuration object to specify the chart
-     */
-    _getChartConfig: function () {
-        return {
-            chart: {
-                zoomType: 'xy'
-            },
-            title: {
-                text: 'Project Cumulative Flow'
-            },
-            xAxis: {
-                tickmarkPlacement: 'on',
-                tickInterval: 20,
+            chartConfig: {
+                chart: {
+                    zoomType: 'xy'
+                },
                 title: {
-                    text: 'Date'
-                }
-            },
-            yAxis: [
-                {
+                    text: 'Project Cumulative Flow'
+                },
+                xAxis: {
+                    tickmarkPlacement: 'on',
+                    tickInterval: 20,
                     title: {
-                        text: 'Count'
-                    }
-                }
-            ],
-            plotOptions: {
-                series: {
-                    marker: {
-                        enabled: false
+                        text: 'Date'
                     }
                 },
-                area: {
-                    stacking: 'normal'
+                yAxis: [
+                    {
+                        title: {
+                            text: 'Count'
+                        }
+                    }
+                ],
+                plotOptions: {
+                    series: {
+                        marker: {
+                            enabled: false
+                        }
+                    },
+                    area: {
+                        stacking: 'normal'
+                    }
                 }
             }
-        };
+        });
     }
 });
